2251199e1b40c8ea620c1010559445a032899c30
Vasilis Tsiligiannis <acinonyx@openwrt.gr>
2021-10-20 00:13:22 +0300

    refresh-requirements: Exclude both hyphen and underscore on 'pkg-resources'
    Signed-off-by: Vasilis Tsiligiannis <acinonyx@openwrt.gr>

--- contrib/refresh-requirements.sh
+++ contrib/refresh-requirements.sh
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-EXCLUDE_REGEXP="^\(pkg-resources\|satnogs-network\)"
+EXCLUDE_REGEXP="^\(pkg[-_]resources\|satnogs-network\)"
 COMPATIBLE_REGEXP=""
 VIRTUALENV_DIR=$(mktemp -d)
 PIP_COMMAND="$VIRTUALENV_DIR/bin/pip"
